d04d73cf2ecbbdd94845920e2482bc1b801e721a
Zee <zjbulllock@gmail.com>
2019-03-23 09:37:48 -0400

    created data scraper to parse value of great value milk directly from page

--- service/milk.go
+++ service/milk.go
@@ -26,7 +26,7 @@ func NewMilk(l loggo.Logger, wally repository.Wally, sms repository.SMS) Milk {
 	}
 }
 
-const goatMilk = "47094270"
+const greatValue = "Great-Value-Whole-Milk-1-Gallon-128-Fl-Oz/10450114"
 
 func (m *milk) SendMilk(who model.Who) (resp *string, err error) {
 	secrets, err := helper.GetSecrets(m.log)
@@ -34,7 +34,7 @@ func (m *milk) SendMilk(who model.Who) (resp *string, err error) {
 		return nil, errors.Wrapf(err, "error getting secrets")
 	}
 
-	milk, err := m.wally.GetMilkPrice(goatMilk, secrets.Wally)
+	milk, err := m.wally.GetMilkPrice(greatValue, secrets.Wally)
 	if err != nil {
 		return nil, errors.Wrapf(err, "error getting milk price")
 	}
